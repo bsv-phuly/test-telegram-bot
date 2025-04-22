f703a3c4ba76d32efe1a0b4ad87473c51cdf6fd8
Ly Phu <phuly@bravesoft.com.vn>
2025-04-22 11:54:48 +0700

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 // vite.config.js
+import { exec } from "child_process";
 import { defineConfig, loadEnv } from "vite";
+
+function openFirebaseHostingUrl() {
+    return {
+        name: 'open-firebase-after-build',
+        buildEnd() {
+            const url = 'https://test-bot-app-799e1.web.app'
+            const openCommand = `start ${url}`
+            exec(openCommand)
+        }
+    }
+}
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
+        plugins: [openFirebaseHostingUrl()],
         define: {
             'process.env.LANGBASE_API_KEY': JSON.stringify(env.LANGBASE_API_KEY),
             'process.env.BOT_TOKEN': JSON.stringify(env.BOT_TOKEN),
