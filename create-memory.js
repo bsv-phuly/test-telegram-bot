f65b47631488ab0cf37619ecaa3f6a8ea769d4ee
bsv-phuly <116058361+bsv-phuly@users.noreply.github.com>
2025-04-23 07:50:40 +0000

    Deploying to gh-pages from @ bsv-phuly/test-telegram-bot@34e5c5bf4a34f95b686ae7b481f6c08bb1233316 🚀

---
+++ create-memory.js
@@ -0,0 +1,14 @@
+import { Langbase } from 'langbase';
+const langbase = new Langbase({
+    apiKey: process.env.LANGBASE_API_KEY,
+});
+async function main() {
+    const memory = await langbase.memories.create({
+        name: 'knowledge-base',
+        description: 'An AI memory for agentic memory workshop',
+        // embedding_model: 'google:text-embedding-004'
+        embedding_model: 'openai:text-embedding-3-large'
+    });
+    console.log('AI Memory:', memory);
+}
+main();
