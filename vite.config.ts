3203329adc18860809886d18e10f7cabe74af8a5
Ly Phu <phuly@bravesoft.com.vn>
2025-04-24 09:12:05 +0700

    add vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env.LANGBASE_API_KEY': JSON.stringify(env.LANGBASE_API_KEY),
+            'process.env.BOT_TOKEN': JSON.stringify(env.BOT_TOKEN),
+            'process.env.CONVEX_DEPLOYMENT': JSON.stringify(env.CONVEX_DEPLOYMENT),
+            'process.env.CONVEX_URL': JSON.stringify(env.CONVEX_URL),
+        },
+        entry: ["/src/main.tsx"],
+    };
+});
